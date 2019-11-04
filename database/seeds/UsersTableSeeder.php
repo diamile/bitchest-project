6160e1d69be0aa9be0b2422c0a601f32fb9c42c9
sarr <diamilesarr2006@gmail.com>
2019-11-04 14:44:46 +0100

    creation de mes utilisateurs grace aux seeders

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+
+    {
+        // $users = factory(App\User::class, 4)->create();
+        
+        DB::Table('users')->insert(array([
+            'name' => 'Diamile',
+            'email' => 'diamilesarr2006@gmail.com',
+            'password' => Hash::make('admin'),
+            'admin' => rand(0, 1)
+        ],
+        [
+            'name' => 'Gerome',
+            'email' => 'gerome@live.fr',
+            'password' => Hash::make('gerome'),
+            'admin' => rand(0, 1)
+        ],
+        [
+            'name' => 'Michael',
+            'email' => 'micheal@gmail.fr',
+            'password' => Hash::make('micheal'),
+            'admin' => rand(0, 1)
+        ],
+        [
+            'name' => 'George',
+            'email' => 'george@bla.fr',
+            'password' => Hash::make('george'),
+            'admin' => rand(0, 1)
+        ]));
+    }
+}
