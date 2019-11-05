d5f3e27e97b409ce696efa9c27e364033333c17a
sarr <diamilesarr2006@gmail.com>
2019-11-05 13:01:07 +0100

    recuperation des données qui se trouvent dans chanque portefeuile de chaque utilisateur

--- app/Http/Controllers/walletsController.php
+++ app/Http/Controllers/walletsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 use App\Currency;
 use App\Wallet;
 use App\User;
@@ -21,8 +21,30 @@ class walletsController extends Controller
 
     public function index(){
         $title = 'Mon portefeuille';
+        //recuperation  de l'utilisateur qui s'est connecté
+        $users = User::where('id', Auth::id())->get();
+
+        //recuperation des du portefeuille de l'utlisateur qui s'est connécté
+        $wallets = Wallet::where('user_id', Auth::id())->get();
+
+        //liste des achats
+        $bought_list=[];
+
+        $bought=[];
+
+        $total_wallet=0;
+
+        foreach($wallets as $wallet){
+            
+         $currency=Currency::where('id',$wallet->crypto_id)->first();
+           
+        }
+
+
 
         
-        return view('customer.wallet',compact('title'));
+
+        
+        return view('customer.wallet',compact('title','users'));
     }
 }
--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -1,1 +1,26 @@
-<h1>hello i' am a  admin</h1>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+
+            @include('admin.layouts.partials._sidenav')
+
+            <div class="col-md-8">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><h1>{{$title}}</h1></div>
+
+                    <div class="panel-body">
+
+                       @if(Session::has('flash_message'))
+                            <div class="alert alert-success">
+                                {{ Session::get('flash_message') }}
+                            </div>
+                        @endif
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
