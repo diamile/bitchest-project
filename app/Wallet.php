e4fc93e68094af026a022c962ad13ac257ec1187
sarr <diamilesarr2006@gmail.com>
2019-11-05 09:09:09 +0100

    relations entre la table wallets et les tables users et currencies

--- app/Wallet.php
+++ app/Wallet.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Wallet extends Model
 {
-    //
+    protected $fillable=[
+      'user_id',
+      'quantity',
+      'crypto_id'
+    ];
+
+
+    public function user(){
+        return $this->hasMany('App\User');
+    }
+
+    public function currency(){
+        return $this->hasOne('App\Currency','id');
+    }
+
 }
