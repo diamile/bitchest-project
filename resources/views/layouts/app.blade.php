9393a4ed7294edcc3189f4c8616406a9d6c6e27a
sarr <diamilesarr2006@gmail.com>
2019-11-08 15:23:23 +0100

    utilisation de bootswach afin de faire le design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <style>
     
     nav{
