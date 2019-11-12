a3b42f99156bd86594e485e73e594a9a9eedc3f4
sarr <diamilesarr2006@gmail.com>
2019-11-12 18:45:29 +0100

    affichage des utilisateurs par ordre decroissant en utilisant les query builder

--- app/Http/Controllers/usersController.php
+++ app/Http/Controllers/usersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use DB;
 
 class usersController extends Controller
 {
@@ -18,7 +19,8 @@ class usersController extends Controller
      public function index(){
          $title = 'Liste de nos Clients';
          
-         $users = User::paginate(6);
+        //  $users = User::paginate(6);
+        $users=DB::table('users')->orderBy('created_at','DESC')->paginate(6);
         
          $statut = ['Aministrateur', 'Client'];
         return view('admin/users', compact('title', 'users', 'statut'));
